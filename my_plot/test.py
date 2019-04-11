6c0b3070af77bd56b8e2402d864aec250ef0b560
ocsphylabtop <ocsphy@qq.com>
2019-04-12 00:44:25 +0800

    构造了泡泡图

--- my_plot/test.py
+++ my_plot/test.py
@@ -6,69 +6,60 @@ Author:Ocsphy
 Date:2019/3/30 0:11
 '''
 
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
 import matplotlib.ticker as mtick
-from pylab import mpl
-
-data = pd.read_excel("jd.xlsx", header=0)
-print(data)
-
-co = np.array(data['names'])
-av_m = np.array(data['av_m']) * 100
-gmv = np.array(data['GMV'])
-
-gmv_grow = np.array(data['GMVgrowth']) * 100
-
-cnames = [
-    '#F0F8FF',
-    '#FAEBD7',
-    '#00FFFF',
-    '#7FFFD4',
-    '#F0FFFF',
-    '#F5F5DC',
-    '#FFE4C4',
-    '#000000',
-    '#FFEBCD',
-    '#0000FF',
-    '#8A2BE2',
-    '#A52A2A',
-    '#DEB887',
-    '#5F9EA0',
-    '#7FFF00',
-    '#D2691E',
-    '#FF7F50',
-    '#6495ED',
-    '#FFF8DC',
-    '#DC143C']
-
-fig, ax = plt.subplots(figsize = (9,6))
-
-n = data.shape[0]
-for i in range(n):
-    ax.scatter(av_m[i], gmv_grow[i], s=gmv[i] / 10000000, c = cnames[-i],
-                alpha=0.8, edgecolors='black')
-    plt.scatter(av_m[i], gmv_grow[i], c = cnames[-i], edgecolors= cnames[-i] ,label = co[i])
-    plt.legend(loc=0)
 
 
-plt.ylabel("销售额同比增长")
-plt.xlabel("平均市场规模")
+def bubles(path, modify=1000000, fontsize=15, percentage=True, **kwargs):
+    data = pd.read_excel(path, header=0)
+    label = np.array(data.iloc[:, 0])
+    x = np.array(data.iloc[:, 1]) * 100
+    y = np.array(data.iloc[:, 2]) * 100
+    size = np.array(data.iloc[:, 3])
+    # set limit
+    if 'ylim' in kwargs.keys():
+        lim = kwargs['ylim']
+        plt.ylim((lim[0], lim[1]))
+    if 'xlim' in kwargs.keys():
+        lim = kwargs['ylim']
+        plt.xlim((lim[0], lim[1]))
 
-mpl.rcParams['font.sans-serif'] = ['KaiTi']
+    n = x.shape[0]
+    for i in range(n):
+        ax.scatter(x[i], y[i], s=size[i] / modify, c=sns.color_palette("hls", n)[i],
+                   alpha=0.8, edgecolors='white')
+        plt.scatter(x[i], y[i], c=sns.color_palette("hls", n)[i], alpha=0.8, s=100,
+                    edgecolors=sns.color_palette("hls", n)[i], label=label[i])
+        plt.legend(loc=0, fontsize=fontsize)
+
+    # set stick
+    name = data.columns.values.tolist()
+    plt.xlabel(name[1], fontsize=fontsize)
+    plt.ylabel(name[2], fontsize=fontsize)
+
+    if percentage:
+        # show in percentage
+        fmt = '%2.0f%%'
+        yticks = mtick.FormatStrFormatter(fmt)
+        ax.yaxis.set_major_formatter(yticks)
+        ax.xaxis.set_major_formatter(yticks)
+
+    ax.spines['top'].set_visible(False)  # 去掉上边框
+    ax.spines['right'].set_visible(False)  # 去掉左边框
 
 
-fmt = '%2.0f%%'
-yticks = mtick.FormatStrFormatter(fmt)
+if __name__ == '__main__':
+    # load data
+    path = "C:/Users/ASUS/Desktop/files/data/医药保健.xlsx"
+    # initialize picture
+    fig, ax = plt.subplots(figsize=(9, 6))
+    mpl.rcParams['font.sans-serif'] = ['KaiTi']
 
-ax.yaxis.set_major_formatter(yticks)
-ax.xaxis.set_major_formatter(yticks)
-plt.ylim((-50, 200))
-plt.xlim((-5, 60))
-
-ax.spines['top'].set_visible(False) #去掉上边框
-ax.spines['right'].set_visible(False) #去掉左边框
-plt.show()
-
-
+    # plot
+    bubles(path, modify=5000, fontsize=15)
+    # plt.savefig('./test2.jpg', dpi=300)
+    plt.show()
