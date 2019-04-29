8c0273252b3411cc1b947d6ffca862dbdd9ffc47
OcsphyDesktop <ocsphy@qq.com>
2019-04-29 11:56:05 +0800

    dont know what happend

--- my_plot/my_plot.py
+++ my_plot/my_plot.py
@@ -1,23 +1,62 @@
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
+import numpy as np
+import pandas as pd
+
+
+
 """
 h
 """
 
-class my_plot:
-    def __init__(self, nrows=1, ncols=1):
-        self.fig, self.axes = plt.subplots(nrows, ncols)
 
-    def plot(self, rows, cols, x, y):
-        axes = self.axes[rows][cols]
-        axes.plot(x, y)
-        return axes
+# class my_plot:
+#     def __init__(self, nrows=1, ncols=1):
+#         self.fig, self.axes = plt.subplots(nrows, ncols)
+#
+#     def plot(self, rows, cols, x, y):
+#         axes = self.axes[rows][cols]
+#         axes.plot(x, y)
+#         return axes
+#
+#     def hist(self):
+
+
+def stacked_bar(axes,x,y):
+
+    """
+    堆叠条形图
+    输入：axes:轴对象
+         x:array_like,横坐标
+         y:DataFrame,需要堆叠的数据
+
+    """
+    if x.shape[0] != y.shape[0]:
+        print("必须输入同样长度的数据")
+        return None
+
+    bottom = 0
+    for label,col in zip(x,y.columns):
+
+        axes.bar(
+            list(range(len(x))),
+            y[col],
+            width=0.5,
+            alpha=0.5,
+            label='label',
+            bottom=bottom,
+            linewidth=0.8,
+            # facecolor='gray',
+            # edgecolor='black'
+                )
+        bottom +=  y[col]
+
 
 
 if __name__ == '__main__':
-    my_plot = my_plot(2, 2)
-    x = [1, 2, 3, 4]
-    y = [1, 2, 3, 4]
-    axes = my_plot.plot(0, 0, x, y)
+    fig,axes = plt.subplots(2,1)
+    data = pd.read_excel("test.xlsx", skiprows=[1], usecols=list(range(11)))
+    stacked_bar(axes[0],data["公司名称"],data["PE"])
+
+
     plt.show()
-...
+
--- my_plot/test.py
+++ my_plot/test.py
@@ -6,13 +6,8 @@ Author:Ocsphy
 Date:2019/3/30 0:11
 '''
 
-from my_plot import my_plot
-import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    my_plot = my_plot(2, 2)
-    x = [1, 2, 3, 4]
-    y = [1, 2, 3, 4]
-    my_plot.plot(0, 0, x, y)
-    my_plot.plot(1, 0, x, y)
-    plt.show()
+import pandas as pd
+
+data = pd.read_excel("test.xlsx")
+print(data)
